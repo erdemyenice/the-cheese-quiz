dc61d8a6a882ad7ac3ddec62594da5db90595734
erdemyenice <yenicee@gmail.com>
2025-04-26 09:59:10 +0100

    Update quiz.js

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,1 +1,17 @@
+export default function Quiz() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#fef9c3',
+      fontFamily: 'sans-serif',
+      padding: '20px',
+      textAlign: 'center'
+    }}>
+      <h1 style={{ fontSize: '2rem' }}>🧀 Let's Find Your Cheese!</h1>
+    </div>
+  );
+}
 
